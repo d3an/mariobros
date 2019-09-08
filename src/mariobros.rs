c9edac466b80cc75bccf0aeb45173022cbfc67b1
d3an <jabury@sympatico.ca>
2019-09-08 01:24:23 -0400

    loaded spritesheet, currently working on map

--- src/mariobros.rs
+++ src/mariobros.rs
@@ -18,6 +18,31 @@ pub const BRICK_HEIGHT: f32 = 18.0;
 
 /* 
 =================================================================
+====================== Static Map Structs =======================
+=================================================================
+*/
+
+pub struct BrickFloor {
+	pub length: f32,
+	pub height: f32,
+}
+
+// Consider adding neighbour fields
+pub struct FloatingFloor {
+	pub top: f32,
+	pub left: f32,
+}
+
+pub struct LargePOW {}
+pub struct MediumPOW {}
+pub struct SmallPOW {}
+
+pub struct LargePipe {}
+pub struct SmallPipe {}
+
+
+/* 
+=================================================================
 ====================== MarioBros Struct =========================
 =================================================================
 */
@@ -89,21 +114,79 @@ fn initialize_camera(world: &mut World) {
 		.build();
 }
 
+// Initializes arena for default game
+fn initialize_map(world: &mut World, sprite_sheet: Handle<SpriteSheet>) {
+	let brick_floor = SpriteRender {
+
+	};
+
+	world
+		.create_entity()
+		.with(brick_floor.clone())
+		.build();
+}
+
 // Add Luigi entity if needed
-fn initialize_players(world: &mut World) {
+fn initialize_players(world: &mut World, sprite_sheet: Handle<SpriteSheet>) {
 	let mut mario_transform = Transform::default();
 
 	// Correctly position the player
 	let top = ARENA_HEIGHT - BRICK_HEIGHT;
 	mario_transform.set_translation_xyz(ARENA_WIDTH * 0.5 - PLAYER_WIDTH * 0.5, top, 0.0);
 
+	let sprite_render = SpriteRender {
+		sprite_sheet: sprite_sheet.clone(),
+		sprite_number: 11, // Default mario is at index 11 in .ron
+	};
+
 	// Create a Mario entity
 	world
 		.create_entity()
+		.with(sprite_render.clone())
 		.with(Paddle::new(Character::Mario))
 		.with(mario_transform)
 		.build();
 }
+
+
+fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
+	// Load the sprite sheet necessary to render the graphics.
+    // The texture is the pixel data
+    // `texture_handle` is a cloneable reference to the texture
+    let texture_handle = {
+    	let loader = world.read_resource::<Loader>();
+    	let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+    	loader.load(
+    		"texture/mario_bros_nes_sprite_sheet.png",
+    		ImageFormat::default(),
+    		(),
+    		&texture_storage,
+    	)
+    };
+
+    let loader = world.read_resource::<Loader>();
+    let texture_storage = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+    	"texture/mario_bros_spritesheet.ron",
+    	SpriteSheetFormat(texture_handle),
+    	(),
+    	&sprite_sheet_store,
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
